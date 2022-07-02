ab489f992c4d19195f1f8fe52133de4820164864
Juan Diego <juandiegomariscal@gmail.com>
2022-07-02 19:59:06 +0200

    feat: Añadido dithering.py

---
+++ dithering.py
@@ -0,0 +1,28 @@
+from PIL import Image, ImageOps
+import sys
+
+try:
+    img = Image.open("./img/a.png")
+    img = ImageOps.grayscale(img)
+
+except:
+    print("No se pudo cargar la imagen")
+    sys.exit(1)
+
+ancho, alto = img.size
+pixels = img.load()
+threshold = 128
+error = 0
+for x in range(0,ancho):
+    for y in range (0,alto):
+        pixels[x,y] += error
+        if pixels[x, y] < threshold:
+            new_pixel = 0
+            error = pixels[x,y]
+            pixels[x, y] = new_pixel 
+        else:
+            new_pixel = 255
+            error = pixels[x,y]-255
+            pixels[x,y] = new_pixel
+img.show()
+#img.save("2.png", "png")
