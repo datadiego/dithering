37e141e7bb18ebcf2b1bc03757867e23b7c1aae8
Juan Diego <juandiegomariscal@gmail.com>
2022-07-02 21:21:12 +0200

    refactor: 🗃️ creada funcion diffusion_error_dithering

--- dithering.py
+++ dithering.py
@@ -1,28 +1,34 @@
+import string
 from PIL import Image, ImageOps
 import sys
 
-try:
-    img = Image.open("./img/a.png")
-    img = ImageOps.grayscale(img)
+def diffusion_error_dithering(img_path, threshold):
+    try:
+        img = Image.open(img_path)
+        img = ImageOps.grayscale(img)
+    except:
+        print("No se pudo cargar la imagen")
+        sys.exit(1)
 
-except:
-    print("No se pudo cargar la imagen")
-    sys.exit(1)
+    ancho, alto = img.size
+    pixels = img.load()
+    error = 0
+    for x in range(0,ancho):
+        for y in range (0,alto):
+            pixels[x,y] += error
+            if pixels[x, y] < threshold:
+                new_pixel = 0
+                error = pixels[x,y]
+                pixels[x, y] = new_pixel 
+            else:
+                new_pixel = 255
+                error = pixels[x,y]-255
+                pixels[x,y] = new_pixel
+    img.show()
 
-ancho, alto = img.size
-pixels = img.load()
-threshold = 128
-error = 0
-for x in range(0,ancho):
-    for y in range (0,alto):
-        pixels[x,y] += error
-        if pixels[x, y] < threshold:
-            new_pixel = 0
-            error = pixels[x,y]
-            pixels[x, y] = new_pixel 
-        else:
-            new_pixel = 255
-            error = pixels[x,y]-255
-            pixels[x,y] = new_pixel
-img.show()
+
+if __name__ == "__main__":
+    path_og = "./img/a.png"
+    a = diffusion_error_dithering(path_og, 128)
+
 #img.save("2.png", "png")
