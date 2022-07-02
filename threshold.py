22ccf95c91d3d010617f9552b2bbc17baaa5bc7e
Juan Diego <juandiegomariscal@gmail.com>
2022-07-02 19:57:27 +0200

    feat: Threshold creado

---
+++ threshold.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageOps
+import sys
+
+try:
+    img = Image.open("./img/a.png")
+    img = ImageOps.grayscale(img)
+
+except:
+    print("No se pudo cargar la imagen")
+    sys.exit(1)
+
+ancho, alto = img.size
+pixels = img.load()
+threshold = 128
+for x in range(0,ancho):
+    for y in range (0,alto):
+        if pixels[x, y] < threshold:
+            new_pixel = 0
+            pixels[x, y] = new_pixel 
+        else:
+            new_pixel = 255
+            pixels[x,y] = new_pixel
+img.show()
+#img.save("2.png", "png")
